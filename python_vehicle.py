c843283676603881e29b92bbce08b5191ced7055
Samroberts9 <126840297+Samroberts9@users.noreply.github.com>
2023-05-01 20:43:38 -0400

    Create python_vehicle.py

---
+++ python_vehicle.py
@@ -0,0 +1,25 @@
+"""
+    Name: python_vehicle.py
+    Author: Samantha Roberts
+    Created: 5.1.23
+"""
+
+class Spaceship():
+
+    def __init__(self):
+        self._name
+        self._species
+        self._destination
+        self._speed
+
+    def launch(self):
+        # Launches and sets the speed to 0
+        pass
+    def accelerate(self):
+        # Increases speed of spaceship
+        pass
+    def decelerate(self):
+        # Reduces speed of spaceship
+        pass
+    def destination(self):
+        # Menu of destinations
